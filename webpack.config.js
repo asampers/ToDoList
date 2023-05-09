953fd6cb9cf1cc7a7ce9cc70cce6adc5db842896
nanarama <annasampers@gmail.com>
2023-05-09 15:33:34 -0500

    Initial changes for Bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/js/index.js',
+  devtool: 'inline-source-map',
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html' 
@@ -14,6 +15,7 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
     module: {
     rules: [
