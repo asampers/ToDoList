a32d53c9398a9763469041a73b2e7acd1534a8ec
nanarama <annasampers@gmail.com>
2023-05-10 13:45:56 -0500

    Add new task form

--- src/js/index.js
+++ src/js/index.js
@@ -3,10 +3,6 @@ import '../scss/styles.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+
 import { ProjectFactory } from './project'
 import { ToDoFactory } from './todo'
-console.log('This was set up properly.')
-let toDo = ToDoFactory('title', 'desc', 'due', 'low')
-let project = ProjectFactory('This is a project', 'This is its desc')
-console.log (toDo)
-console.log(project)
