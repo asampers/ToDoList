be463678f105ef298b0030c2931ae21c10bced0a
nanarama <annasampers@gmail.com>
2023-05-10 17:20:40 -0500

    Add createToDo to task btn

--- src/js/index.js
+++ src/js/index.js
@@ -5,4 +5,13 @@ import '../scss/styles.scss'
 import * as bootstrap from 'bootstrap'
 
 import { ProjectFactory } from './project'
-import { ToDoFactory } from './todo'
+import { createToDo } from './todo'
+
+const newTask = document.querySelector("#new-task")
+newTask.addEventListener('submit', (event) => {
+  event.preventDefault();
+  let task = createToDo(event);
+  console.log(task);
+  newTask.reset();
+  document.querySelector('.new-task-btn').click();
+})
--- src/js/todo.js
+++ src/js/todo.js
@@ -1,11 +1,18 @@
-const ToDoFactory = (title, description, dueDate, priority, completed=false) => {
-  let index = 0;
-  
-  const setIndex = () => {
-    index++;
-  }
-  
-  return { setIndex, title, description, dueDate, priority, completed };
+let counter = 0
+
+const ToDoFactory = (title, description, dueDate, priority, completed=false, index=counter++ ) => {
+  return { index, title, description, dueDate, priority, completed };
 }
 
-export { ToDoFactory };
+const createToDo = (event) => {
+  let formValue = event.target.elements;
+	
+  const title = formValue.title.value;
+	const desc = formValue.description.value;
+	const dueDate = formValue.date.value;
+  const priority = formValue.priority.value;
+
+  return ToDoFactory(title, desc, dueDate, priority);
+}
+
+export { createToDo };
