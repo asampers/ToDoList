9e86ea7f44d94e5f2dca6da37e8d66e2e3cb2ff0
nanarama <annasampers@gmail.com>
2023-06-08 17:08:23 -0500

    Add edit button, refactor viewField and hiddenField into their own functions

--- src/js/todoUI.js
+++ src/js/todoUI.js
@@ -9,38 +9,55 @@ const TodoUI = (() => {
     todoList.appendChild(row);
   };
 
-  const fillRow = (todo, row) => {
+  const createViewField = (todo) => {
+    const viewField = document.createElement("div");
     const checkbox = document.createElement("input");
     const title = document.createElement("span");
     const dueDate = document.createElement("span");
     const priority = document.createElement("span");
     const expand = document.createElement("button");
-    const viewField = document.createElement("div");
-    const hiddenField = document.createElement("div");
-    //const moreInfo = document.createElement('div');
 
+    viewField.classList.add("d-flex", "justify-content-between");
     expand.innerHTML = "<ion-icon name='chevron-down-outline'></ion-icon>";
     expand.classList.add("btn", "btn-sm", "btn-outline-info");
     expand.setAttribute("data-bs-target", `#todo-${todo.index}`);
     expand.setAttribute("data-bs-toggle", "collapse");
     checkbox.type = "checkbox";
     checkbox.checked = todo.completed;
-    viewField.classList.add("d-flex", "justify-content-between");
-    hiddenField.id = `todo-${todo.index}`;
-    hiddenField.classList.add("collapse");
 
     title.textContent = todo.title;
     dueDate.textContent = todo.dueDate;
     priority.textContent = todo.priority;
-    hiddenField.textContent = `Description: ${todo.description || "none"}`;
 
     viewField.appendChild(checkbox);
     viewField.appendChild(title);
     viewField.appendChild(dueDate);
     viewField.appendChild(priority);
     viewField.appendChild(expand);
-    row.appendChild(viewField);
-    row.appendChild(hiddenField);
+
+    return viewField;
+  };
+
+  const createHiddenField = (todo) => {
+    const hiddenField = document.createElement("div");
+    const description = document.createElement("span");
+    const edit = document.createElement("button");
+
+    hiddenField.id = `todo-${todo.index}`;
+    hiddenField.classList.add("collapse", "ps-4");
+    edit.classList.add("btn");
+    edit.innerHTML = "<ion-icon name='create-outline'></ion-icon>";
+    description.textContent = `Description: ${todo.description || "none"}`;
+
+    hiddenField.appendChild(description);
+    hiddenField.appendChild(edit);
+
+    return hiddenField;
+  };
+
+  const fillRow = (todo, row) => {
+    row.appendChild(createViewField(todo));
+    row.appendChild(createHiddenField(todo));
   };
 
   return { createRow };
