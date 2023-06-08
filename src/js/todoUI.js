3e09f24d14ac22c181cf5dae98d53702c005a002
nanarama <annasampers@gmail.com>
2023-06-08 16:43:55 -0500

    Collapse is working!

--- src/js/todoUI.js
+++ src/js/todoUI.js
@@ -2,51 +2,45 @@ const TodoUI = (() => {
   const createRow = (todo) => {
     const todoList = document.querySelector(".todo-list");
     const row = document.createElement("div");
-    const hiddenRow = document.createElement("div");
 
-    row.classList.add(
-      "d-flex",
-      "justify-content-between",
-      "mb-3",
-      "border",
-      "rounded",
-      "p-2"
-    );
-    row.id = `accordion${todo.index}`;
-    hiddenRow.classList.add("accordion-collapse", "collapse");
-    hiddenRow.setAttribute("data-bs-parent", `#accordion${todo.index}`);
-    hiddenRow.id = `collapse${todo.index}`;
-    fillRow(todo, row, hiddenRow);
+    row.classList.add("mb-3", "border", "rounded", "p-2");
+
+    fillRow(todo, row);
     todoList.appendChild(row);
-    todoList.appendChild(hiddenRow);
   };
 
-  const fillRow = (todo, row, hiddenRow) => {
+  const fillRow = (todo, row) => {
     const checkbox = document.createElement("input");
     const title = document.createElement("span");
     const dueDate = document.createElement("span");
     const priority = document.createElement("span");
-    const expand = document.createElement("span");
-    const hiddenField = document.createElement("span");
+    const expand = document.createElement("button");
+    const viewField = document.createElement("div");
+    const hiddenField = document.createElement("div");
     //const moreInfo = document.createElement('div');
 
     expand.innerHTML = "<ion-icon name='chevron-down-outline'></ion-icon>";
-    hiddenField.classList.add("accordion-body");
-    hiddenField.setAttribute("colspan", "5");
+    expand.classList.add("btn", "btn-sm", "btn-outline-info");
+    expand.setAttribute("data-bs-target", `#todo-${todo.index}`);
+    expand.setAttribute("data-bs-toggle", "collapse");
     checkbox.type = "checkbox";
     checkbox.checked = todo.completed;
+    viewField.classList.add("d-flex", "justify-content-between");
+    hiddenField.id = `todo-${todo.index}`;
+    hiddenField.classList.add("collapse");
 
     title.textContent = todo.title;
     dueDate.textContent = todo.dueDate;
     priority.textContent = todo.priority;
-    hiddenField.textContent = `Description: ${todo.description}`;
+    hiddenField.textContent = `Description: ${todo.description || "none"}`;
 
-    row.appendChild(checkbox);
-    row.appendChild(title);
-    row.appendChild(dueDate);
-    row.appendChild(priority);
-    row.appendChild(expand);
-    hiddenRow.appendChild(hiddenField);
+    viewField.appendChild(checkbox);
+    viewField.appendChild(title);
+    viewField.appendChild(dueDate);
+    viewField.appendChild(priority);
+    viewField.appendChild(expand);
+    row.appendChild(viewField);
+    row.appendChild(hiddenField);
   };
 
   return { createRow };
