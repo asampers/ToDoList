38f5ac7cec77e3a97ffe10e685e3cfbe79708708
nanarama <annasampers@gmail.com>
2023-06-09 12:07:20 -0500

    Rework edit btn layout

--- src/js/todoUI.js
+++ src/js/todoUI.js
@@ -16,24 +16,34 @@ const TodoUI = (() => {
     const dueDate = document.createElement("span");
     const priority = document.createElement("span");
     const expand = document.createElement("button");
+    const edit = document.createElement("button");
+    const btnDiv = document.createElement("div");
+    const titleCheckDiv = document.createElement("div");
 
     viewField.classList.add("d-flex", "justify-content-between");
-    expand.innerHTML = "<ion-icon name='chevron-down-outline'></ion-icon>";
+    checkbox.type = "checkbox";
+    checkbox.classList.add("me-3");
     expand.classList.add("btn", "btn-sm", "btn-outline-info");
     expand.setAttribute("data-bs-target", `#todo-${todo.index}`);
     expand.setAttribute("data-bs-toggle", "collapse");
-    checkbox.type = "checkbox";
-    checkbox.checked = todo.completed;
+    edit.classList.add("btn", "btn-sm", "btn-outline-success", "ms-3");
 
+    checkbox.checked = todo.completed;
     title.textContent = todo.title;
     dueDate.textContent = todo.dueDate;
     priority.textContent = todo.priority;
+    expand.innerHTML = "<ion-icon name='chevron-down-outline'></ion-icon>";
+    edit.innerHTML = "<ion-icon name='create-outline'></ion-icon>";
 
-    viewField.appendChild(checkbox);
-    viewField.appendChild(title);
+    titleCheckDiv.appendChild(checkbox);
+    titleCheckDiv.appendChild(title);
+    btnDiv.appendChild(expand);
+    btnDiv.appendChild(edit);
+
+    viewField.appendChild(titleCheckDiv);
     viewField.appendChild(dueDate);
     viewField.appendChild(priority);
-    viewField.appendChild(expand);
+    viewField.appendChild(btnDiv);
 
     return viewField;
   };
@@ -41,16 +51,13 @@ const TodoUI = (() => {
   const createHiddenField = (todo) => {
     const hiddenField = document.createElement("div");
     const description = document.createElement("span");
-    const edit = document.createElement("button");
 
     hiddenField.id = `todo-${todo.index}`;
     hiddenField.classList.add("collapse", "ps-4");
-    edit.classList.add("btn");
-    edit.innerHTML = "<ion-icon name='create-outline'></ion-icon>";
-    description.textContent = `Description: ${todo.description || "none"}`;
+
+    description.innerHTML = `<u>Description</u>: ${todo.description || "none"}`;
 
     hiddenField.appendChild(description);
-    hiddenField.appendChild(edit);
 
     return hiddenField;
   };
