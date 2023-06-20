6941bc4a7f64342d1a50c9c1fbfd20deeb182d14
nanarama <annasampers@gmail.com>
2023-06-20 10:06:15 -0500

    Fix padding-top and default project due dates

--- src/js/defaultProject.js
+++ src/js/defaultProject.js
@@ -4,7 +4,7 @@ const defaultProject = () => {
       0: {
         title: "Laundry",
         description: "",
-        dueDate: "2023-05-11",
+        dueDate: "2023-07-11",
         priority: "Medium",
         completed: false,
         index: 0,
@@ -12,7 +12,7 @@ const defaultProject = () => {
       1: {
         title: "Gym",
         description: "leg day!",
-        dueDate: "2023-05-11",
+        dueDate: "2023-07-11",
         priority: "High",
         completed: false,
         index: 1,
@@ -20,7 +20,7 @@ const defaultProject = () => {
       2: {
         title: "Tan",
         description: "how we do in the O.C.",
-        dueDate: "2023-05-11",
+        dueDate: "2023-07-11",
         priority: "",
         completed: false,
         index: 2,
